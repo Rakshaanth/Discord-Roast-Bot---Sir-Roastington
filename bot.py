5f8655482d57c1b667e597e7e25d68c51daf9e3f
Rakshaanth <Rakshaanth007@gmail.com>
2025-12-18 18:43:41 -0800

    Connction Gemini and testing in Discord Server

--- bot.py
+++ bot.py
@@ -2,42 +2,34 @@ import discord
 from discord import app_commands
 
 import config
-from config import DISCORD_TOKEN # Loads the Discord bot token from config.py
+from config import DISCORD_TOKEN
 
 from Prompts.personality import build_roast_prompt
 
-
-#Intents
-''' Default is enough for bot commands '''
+# Intents
 intents = discord.Intents.default()
 
-# Client, main bot 
+# Client
 class DiscordRoastBot(discord.Client):
     def __init__(self):
-        super().__init__(intents=intents) #call parent class constructor
-        self.tree = app_commands.CommandTree(self) # Create command tree for slash commands
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
-        ''' Sync slash commans of th bot on discord, this makes /ping available '''
     async def setup_hook(self):
         await self.tree.sync()
-    
-    ''' Trigger when bot connects to discord '''
+
     async def on_ready(self):
         print(f'Logged in as {self.user}')
         print('------')
 
-# Create bot instance
 client = DiscordRoastBot()
 
-#test / command
+# /ping command
 @client.tree.command(name="ping", description="Test Command")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("🥁Pong!")
 
-''' Run the bot with the token from config.py, opening connection to Discord '''
-client.run(config.DISCORD_TOKEN)
-
-# Add the /roast command
+# /roast command
 @client.tree.command(
     name="roast",
     description="Get roasted by Sir Roastington"
@@ -54,10 +46,13 @@ async def roast(
     # Build prompt (LLM not used yet)
     prompt = build_roast_prompt(username, about)
 
-    # Dummy response for now
+    # Dummy response
     dummy_roast = (
         f"Ah yes, {username}. "
         "One observes thee and immediately understands why silence was invented."
     )
 
     await interaction.response.send_message(dummy_roast)
+
+# Run the bot (must be last)
+client.run(DISCORD_TOKEN)
